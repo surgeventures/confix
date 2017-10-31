dc2ebf998a9dd6d9e6999c41fd72fd35d150c241
Karol Sluszniak <ksluszniak@gmail.com>
2017-10-31 14:56:42 +0100

    Release 0.4.0: add use, add bang funcs

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Confix.Mixfile do
 
   def project do
     [app: :confix,
-     version: "0.3.0",
+     version: "0.4.0",
      elixir: "~> 1.4",
      elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
