7bc81cfcebf3c7bd23074ba0dbc54d69c43a4822
Kamil Kowalski <kamil@kamilkowalski.pl>
2021-01-22 19:51:37 +0100

    Update excoveralls, configure CI

--- mix.exs
+++ mix.exs
@@ -56,7 +56,7 @@ defmodule Confix.Mixfile do
     [
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.23", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.7", only: :test}
+      {:excoveralls, "~> 0.13", only: :test}
     ]
   end
 
